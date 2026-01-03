3eef19994d2c2748e759f27bbd7a2dd319fb33a0
Caio Alves <caioluizalves1995@gmail.com>
2026-01-03 15:38:47 -0300

    Fix: alterando tipo de storage de local para github

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,12 +2,14 @@ import { articlesKs, authorsKs, categoriesKs } from "@/lib/keystatic";
 import { config } from "@keystatic/core";
 
 export default config({
+  // 1. MUDANÇA ESSENCIAL: Altere de "local" para "github"
   storage: {
-    kind: "local",
+    kind: "github",
+    repo: "caioalves19/central-da-serie-d", // Seu repositório exato
   },
   ui: {
     brand: {
-      name: "Astro News",
+      name: "Central da Série D", // Atualizado para o seu projeto
     },
     navigation: ["---", "articles", "---", "authors", "categories"],
   },
