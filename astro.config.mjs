190c4370b2f21978e1f062da3a83474e94a7105d
Caio Alves <caioluizalves1995@gmail.com>
2026-01-03 15:33:46 -0300

    Fix: alterando output para server para compatibilidade com Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,31 +7,34 @@ import { modifiedTime, readingTime } from "./src/lib/utils/remarks.mjs";
 import { SITE } from "./src/lib/config";
 import keystatic from "@keystatic/astro";
 import react from "@astrojs/react";
-import { loadEnv } from "vite";
 import pagefind from "astro-pagefind";
+import vercel from "@astrojs/vercel";
 
-const { RUN_KEYSTATIC } = loadEnv(import.meta.env.MODE, process.cwd(), "");
+const integrations = [mdx(), sitemap(), pagefind(), react(), keystatic()];
 
-const integrations = [mdx(), sitemap(), pagefind()];
-
-if (RUN_KEYSTATIC === "true") {
-  integrations.push(react());
-  integrations.push(keystatic());
-}
-
-// https://astro.build/config
 export default defineConfig({
   site: SITE.url,
   base: SITE.basePath,
+
+  // Mantemos como "server" para o Keystatic funcionar na Vercel
+  output: "server",
+
   markdown: {
     remarkPlugins: [readingTime, modifiedTime],
   },
+
   image: {
     responsiveStyles: true,
     breakpoints: [640, 1024],
   },
+
   integrations,
+
   vite: {
+    // O comentário abaixo ignora o erro de conflito de tipos do Vite
+    // @ts-ignore
     plugins: [tailwindcss()],
   },
+
+  adapter: vercel(),
 });
