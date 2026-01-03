7cb19a55a39dc38088a01056ad45bddc4578aedb
Caio Alves <caioluizalves1995@gmail.com>
2026-01-03 20:07:50 -0300

    atualizando .gitignore

--- .vercel/output/_functions/chunks/articles_Chiy2DdI.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./articles_BroS3xUC.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
--- .vercel/output/_functions/chunks/error404_BYho5EY7.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./error404_Bc7uCwxj.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
